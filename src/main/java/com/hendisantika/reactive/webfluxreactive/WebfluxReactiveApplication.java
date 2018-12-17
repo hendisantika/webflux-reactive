76ceb311897a94df99d953b1a29cea74e8066db0
Hendi Santika <hendisantika@yahoo.co.id>
2018-12-18 06:29:32 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/reactive/webfluxreactive/WebfluxReactiveApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.reactive.webfluxreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxReactiveApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/hendisantika/reactive/webfluxreactive/WebfluxReactiveApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hendisantika.reactive.webfluxreactive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebfluxReactiveApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
